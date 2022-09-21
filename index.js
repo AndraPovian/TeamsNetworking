9167aba80dbe52403c7abba3757b98947d1fd4a4
AndraPovian <109029420+AndraPovian@users.noreply.github.com>
2022-09-21 17:33:52 +0300

    schimbat denumirile

--- index.js
+++ index.js
@@ -1,59 +1,48 @@
-function loadUsers() {
-  fetch("users.json")
-    .then((r) => r.json())
-    //.then(function (response) {
-    // return response.json();
-    //})
-    .then((users) => {
-      displayUsers(users);
+function loadTeams() {
+  fetch("http://localhost:3000/teams-json")
+    .then((r) => r.json() )
+    .then((teams) => {
+      displayTeams(teams);
     });
 }
 
-function displayUsers(users) {
-  users.map(function (user) {
+function displayTeams(teams) {
+  for (i = 0; i < teams.length; i++) {
+    console.log(teams[i]);
     document.getElementById("tbody").innerHTML += `<tr>
-      <td>${user.name}</td>
-      <td>${user.lastName}</td> 
-      <td><a href="${user.link}">Link</a></td>
-      <td>${user.projectURL}</td>
-      <td>x e </td>
-      </tr>`;
-  });
-  // for (i = 0; i < users.length; i++) {
-  //   document.getElementById("tbody").innerHTML += `<tr>
-  //     <td>${users[i].name}</td>
-  //     <td>${users[i].lastName}</td>
-  //     <td><a href="${users[i].link}">Link</a></td>
-  //     <td>${users[i].projectURL}</td>
-  //     <td>${users[i].x}</td>
-  //     </tr>`;
-  // }
+      <td>${teams[i].promotion}</td>
+      <td>${teams[i].members}</td>
+      <td>${teams[i].name}</td>
+      <td><a href="${teams[i].url}">link</a></td>
+    </tr>`;
+  }
 }
 
 function $(selector) {
   return document.querySelector(selector);
 }
 
-function submitForm(e) {
+function submitform(e) {
   e.preventDefault();
   const promotion = $("input[name=promotion]").value;
-  const Members = $("input[name=Members]").value;
-  const Name = $("input[name=Name]").value;
-  const URL = $("input[name=URL]").value;
+  const members = $("input[name=members]").value;
+  const name = $("input[name=name]").value;
+  const url = $("input[name=url]").value;
 
-  const user = {
+  const team = {
     promotion: promotion,
-    Members: Members,
-    Name: Name,
-    URL: URL,
+    members: members,
+    name: name,
+    url: url,
   };
-  console.warn("adauga in users.json", JSON.stringify(user));
+  console.warn("adauga in teams.json", JSON.stringify(team));
 }
 
 function initEvents() {
-  const form = document.getElementById("EditForm");
-  form.addEventListener("submit", submitForm);
+  const form = document.getElementById("editForm");
+  console.warn("form", form);
+  form.addEventListener("submit", submitform);
 }
 
-loadUsers();
+loadTeams();
 initEvents();
